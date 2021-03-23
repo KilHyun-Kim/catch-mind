a5ec36b1a509e871bf5c57ab78915be9d67209c8
KilHyun-Kim <rlaarlfgus@gmail.com>
2021-03-23 22:02:31 +0900

    Using socket to define chat concept.

--- src/static/index.js
+++ src/static/index.js
@@ -1,5 +1,17 @@
 // static : 프론트엔드를 위한 것
-
 const socket = io("/");
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`You: ${message}`);
+}
 
-socket.on("hello", () => console.log("Somebody said Hello"));
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
